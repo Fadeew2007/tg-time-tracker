49399b7b23c3743a6389214f2012db742d8df660
Fadeew2007 <tarifa93@gmail.com>
2025-02-19 14:59:05 +0200

    config fixed

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,9 @@
 import os
 
-BOT_TOKEN = "7884553220:AAG4MquIKRujYaaAhtG0EuZDta6qGFqL0s"
+# Отримуємо токен бота з середовища або використовуємо значення за замовчуванням
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "7884553220:AAG4MquIKRujYaaAhtG0EuZDta6qGFqL0s")
 API_URL = os.getenv("API_URL", "https://profound-wholeness-production-b760.up.railway.app/api/")
 
-print(f"API_URL: {API_URL}")  # Доданий рядок для перевірки
+# Вивід для перевірки
+print(f"API_URL: {API_URL}")  
+print(f"Telegram Bot Token: {BOT_TOKEN}")  # Дебаг
