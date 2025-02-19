c473dbb02abdf3a8861b0e974fc687aebf363f43
Fadeew2007 <tarifa93@gmail.com>
2025-02-19 14:47:03 +0200

    static

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,6 @@
 import os
 
-BOT_TOKEN = "7884553220:AAG4MquIKRujYQaaAhtG0EuZDta6qGFqL0s"
-API_URL = "http://127.0.0.1:8000/api/"
+BOT_TOKEN = "7884553220:AAG4MquIKRujYaaAhtG0EuZDta6qGFqL0s"
+API_URL = os.getenv("API_URL", "https://profound-wholeness-production-b760.up.railway.app/api/")
+
+print(f"API_URL: {API_URL}")  # Доданий рядок для перевірки
