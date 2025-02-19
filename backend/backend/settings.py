b3117d78460312927703cdad44f071c6eff10a4e
Fadeew2007 <tarifa93@gmail.com>
2025-02-19 02:51:54 +0200

    Fix: Collectstatic before starting server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,4 +134,10 @@ USE_L10N = False
 # -------------------------------
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "backend", "static"),
+]
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
